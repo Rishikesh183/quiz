a74edf3deb600a03663345db854cb95479c5bb19
Rishikesh183 <158394885+Rishikesh183@users.noreply.github.com>
2025-07-22 17:00:36 +0530

    Added a new Quiz

--- src/data/quizzes.ts
+++ src/data/quizzes.ts
@@ -139,6 +139,75 @@ export const quizzes: Quiz[] = [
             "correctAnswer": "Mayank Agarwal"
         }
     ]
+},
+ {
+    "id": "3",
+    "title": "Quiz - 1",
+    "description": "Test Your Cricket knowledege with these diffilcult quiz questions!",
+    "category": "Cricket",
+    "image": "https://superblog.supercdn.cloud/site_cuid_clr6oh1no0006rmr89yhkxgu8/images/chennai-new-1710336746475-compressed.jpeg",
+    "questions": [
+        {
+            "id": "cric-1",
+            "question": "Who is the most recent player who has completed 1000 runs and 100 wickets in odis",
+            "options": ["Wainindu Hasaranga", "Ravi Jadeja", "Hardik Pandya", "Mitchell Marsh"],
+            "correctAnswer": "Wainindu Hasaranga"
+        },
+        {
+            "id": "cric-2",
+            "question": "How many Indian players have taken 100 wickets with 10000 runs in odis",
+            "options": ["1", "2", "3", "4"],
+            "correctAnswer": "2"
+        },
+        {
+            "id": "cric-3",
+            "question": "What is the highest individual score by a player in t20i cricket",
+            "options": ["156", "172", "180", "290"],
+            "correctAnswer": "172"
+        },
+        {
+            "id": "cric-4",
+            "question": "Who scored the highest individual t20i score among Indians",
+            "options": ["Rohit Sharma", "Shubman Gill", "Virat Kohli", "Abhishek sharma"],
+            "correctAnswer": "Abhishek sharma"
+        },
+        {
+            "id": "cric-5",
+            "question": "Which player scored the most runs in a single innings of a t20i game ending up in losing cause",
+            "options": ["Glenn Maxwell", "Rohit Sharma", "Rishab Pant", "Shane Watson"],
+            "correctAnswer": "Shane Watson"
+        },
+        {
+            "id": "cric-6",
+            "question": "How many players have scored century in t20i cricket in losing cause",
+            "options": ["15", "26", "22", "28"],
+            "correctAnswer": "26"
+        },
+        {
+            "id": "cric-7",
+            "question": "In t20i cricket how many instances are there where a player scored 36 runs in a single Over",
+            "options": ["4", "1", "6", "3"],
+            "correctAnswer": "6"
+        },
+        {
+            "id": "cric-8",
+            "question": "Who is the only player from non full member team who hit 6 sixes in at 20i",
+            "options": ["Manan Bashir", "Sikinder Raza", "Shafique Ahmed", "Babar Azam"],
+            "correctAnswer": "Manan Bashir"
+        },
+        {
+            "id": "cric-9",
+            "question": "Who is the only player to score 5000 runs on a single Ground in international cricket [test,t20i,odis]",
+            "options": ["Steven smith", "Mushfiqur Rahim", "Virat Kohli", "Don bradman"],
+            "correctAnswer": "Mushfiqur Rahim"
+        },
+        {
+            "id": "cric-10",
+            "question": "Which player in the history scored most intl runs without a century",
+            "options": ["Vernon Philiander", "James Anderson", "Shane warne", "Waqar Younis"],
+            "correctAnswer": "Shane warne"
+        }
+    ]
 }
 
 ];
