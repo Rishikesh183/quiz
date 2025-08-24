828b8534ead89b1cf3f949abcaf1737700565acd
Rishikesh183 <158394885+Rishikesh183@users.noreply.github.com>
2025-08-24 10:48:58 +0530

    Added new Quiz

--- src/data/quizzes.ts
+++ src/data/quizzes.ts
@@ -2,8 +2,77 @@
 import { Quiz } from "@/types/quiz";
 
 export const quizzes: Quiz[] = [
-  {
+    {
     "id": "1",
+    "title": "Test Trivia",
+    "description": "Test your cricket knowledge with these extremely challenging questions , FT - Test Cricket Trivia",
+    "category": "TEST-Cricket",
+    "image": "https://library.sportingnews.com/styles/twitter_card_120x120/s3/2024-07/GettyImages-1497834098.jpg?itok=CjAuI2zk",
+    "questions": [
+        {
+            "id": "cric-1",
+            "question": "Which player holds the record of scoring most runs in a Test Series",
+            "options": ["Shubman Gill", "Don Bradman", "Sachin Tendulkar", "WR Hammond"],
+            "correctAnswer": "Don Bradman"
+        },
+        {
+            "id": "cric-2",
+            "question": "Which player holds the record of Having highest Strike rate in a Test Innings",
+            "options": ["Moeen Ali", "Colin De Grandhome", "Umesh Yadav", "Ricky Ponting"],
+            "correctAnswer": "Umesh Yadav"
+        },
+        {
+            "id": "cric-3",
+            "question": "Who is the first asian player to score a century on his test debut",
+            "options": ["RH Sodhan", "Moin Khan", "Javed Miandad", "Lala Amarnath"],
+            "correctAnswer": "Lala Amarnath"
+        },
+        {
+            "id": "cric-4",
+            "question": "Who scored most runs in a single day of a test match",
+            "options": ["Don Bradman", "WR Hammond", "Wian Mulder", "V sehwag"],
+            "correctAnswer": "Don Bradman"
+        },
+        {
+            "id": "cric-5",
+            "question": "Which indian player has scored most runs in south africa Tests",
+            "options": ["Sachin Tendulkar", "Virat Kohli", "Rahul Dravid", "Virendra Sehwag"],
+            "correctAnswer": "Sachin Tendulkar"
+        },
+        {
+            "id": "cric-6",
+            "question": "Who Scored most runs in Tied Test Matches",
+            "options": ["Gary Sobers", "Dean Jones", "Kapil Dev", "Norm O'Neill "],
+            "correctAnswer": "Dean Jones"
+        },
+        {
+            "id": "cric-7",
+            "question": "Who is the only indian player to win a player of the match award when his team won the game after following on",
+            "options": ["Rahul Dravid", "Sachin Tendulkar", "VVS Laxman", "Virendra Sehwag"],
+            "correctAnswer": "VVS Laxman"
+        },
+        {
+            "id": "cric-8",
+            "question": "Which team has won most test series by whitewashing the opponent",
+            "options": ["South Africa", "India", "England", "Australia"],
+            "correctAnswer": "Australia"
+        },
+        {
+            "id": "cric-9",
+            "question": "Which is the first team to lose a test match by just 1 run",
+            "options": ["Australia", "India", "New Zealand", "England"],
+            "correctAnswer": "Australia"
+        },
+        {
+            "id": "cric-10",
+            "question": "Which team holds the record of most consecutive test series wins",
+            "options": ["South Africa", "India", "Sri Lanka", "West Indies"],
+            "correctAnswer": "India"
+        }
+    ]
+},
+  {
+    "id": "2",
     "title": "ODI Trivia",
     "description": "Test your cricket knowledge with these extremely challenging questions , FT - ODIs Trivia",
     "category": "ODI-Cricket",
